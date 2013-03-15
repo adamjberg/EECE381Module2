5efba16c7f5f55ddbff2e57a2d8595cdc89b653d
Adam Berg <adam@xyzdigital.com>
2013-03-15 10:23:52 -0700

    Add a Sound struct to hold the raw sound data
    The Sound struct holds the raw sound data and allows playing, pausing,
    looping, and eventually more effects to be added to a specific sound.

---
+++ software/StereoSystem/sound/Sound.c
@@ -0,0 +1,162 @@
+/*
+ * Sound.c
+ *
+ *  Created on: 2013-03-06
+ *      Author: Adam
+ */
+
+#include "Sound.h"
+
+/**
+ * Helper function to read multiple bytes and return the representative int value
+ */
+unsigned int readInt(int file_pointer, int numBytesToRead) {
+	unsigned int ret = 0;
+	int i;
+	unsigned int bytes[numBytesToRead];
+	for (i = 0; i < numBytesToRead; i++) {
+		bytes[i] = alt_up_sd_card_read(file_pointer);
+		ret |= ((unsigned int) bytes[i] << (8 * i));
+	}
+	return ret;
+}
+
+void clearSoundBuffer(struct Sound* sound) {
+	int i;
+	for (i = 0; i < sound->length; i++) {
+		sound->buffer[i] = 0;
+	}
+}
+
+struct Sound* initSound(unsigned int length) {
+	struct Sound* this = (struct Sound*) malloc(sizeof(struct Sound));
+	this->length = length;
+	this->position = 0;
+	this->buffer = (unsigned int*) malloc(sizeof(unsigned int) * length);
+	this->playing = false;
+	clearSoundBuffer(this);
+	return this;
+}
+
+void handleSoundEnd(struct Sound* this) {
+	if (this->loops == 0) {
+		stopSound(this);
+	} else {
+		this->position = 0;
+		if (this->loops > 0)
+			this->loops--;
+	}
+}
+
+/**
+ * Update the position index for this sound.
+ * Determines if the sound is complete and allows it to continue playing if
+ * it should loop.
+ *
+ * @param numWritten - The number of values written to the buffer
+ */
+void updateSoundPosition(struct Sound* this, int numWritten) {
+	if (!this->playing)
+		return;
+	this->position += numWritten;
+
+	if (this->position >= this->length) {
+		handleSoundEnd(this);
+	}
+}
+
+/**
+ * Creates a Sound struct and loads the correct wav file from the SD card
+ *
+ * @param id - id of the sound to load
+ * @param buffer - buffer to save the sound wave into
+ * NOTE: If a NULL buffer is supplied the sound will be saved in the Sound struct
+ */
+struct Sound* loadWavSound(char * filename) {
+	printf("loading sound: %s\n", filename);
+
+	struct Sound* sound;
+	int i, index = 0;
+	int file_pointer = alt_up_sd_card_fopen(filename, false);
+	if (file_pointer < 0) {
+		alt_up_sd_card_fclose(file_pointer); //close the file
+		return false;
+	}
+
+	//Start reading the wav header
+	while (index < BITS_PER_SAMPLE_OFFSET) {
+		alt_up_sd_card_read(file_pointer);
+		index++;
+	}
+
+	int bytes_per_sample = readInt(file_pointer, 2) / BITS_PER_BYTE;
+	index += 2;
+
+	while (index < DATA_LENGTH_OFFSET) {
+		alt_up_sd_card_read(file_pointer);
+		index++;
+	}
+
+	sound = initSound(readInt(file_pointer, 4) / bytes_per_sample);
+
+	printf("length: %u\n", sound->length);
+
+	for (i = 0; i < sound->length; i++) {
+		sound->buffer[i] = readInt(file_pointer, bytes_per_sample);
+	}
+
+	printf("Sound loading complete\n");
+
+	alt_up_sd_card_fclose(file_pointer);
+
+	return sound;
+}
+
+void combineSounds(struct Sound* sound, struct Sound* soundToAdd, int startIndex, int numToWrite, bool overwrite) {
+	int i, indexToWrite = startIndex, indexToRead =
+			soundToAdd->position;
+	for (i = 0; i < numToWrite; i++) {
+		if (indexToWrite >= sound->length) {
+			indexToWrite = 0;
+		}
+		if (indexToRead >= soundToAdd->length) {
+			handleSoundEnd(soundToAdd);
+			if (!soundToAdd->playing) {
+				return;
+			}
+			indexToRead = 0;
+		}
+		if (overwrite) {
+			sound->buffer[indexToWrite] = soundToAdd->buffer[indexToRead];
+		} else {
+			sound->buffer[indexToWrite] += soundToAdd->buffer[indexToRead];
+		}
+		indexToRead++;
+		indexToWrite++;
+	}
+}
+
+void playSound(struct Sound* sound, float volume, int startTime, int loops) {
+	sound->position = startTime;
+	sound->volume = volume;
+	sound->playing = true;
+	sound->loops = loops;
+}
+
+void pauseSound(struct Sound* sound) {
+	sound->playing = false;
+}
+
+void resumeSound(struct Sound* sound) {
+	sound->playing = true;
+}
+
+void stopSound(struct Sound* sound) {
+	sound->playing = false;
+	sound->position = 0;
+}
+
+void unloadSound(struct Sound* sound) {
+	free(sound->buffer);
+	free(sound);
+}
---
+++ software/StereoSystem/sound/Sound.h
@@ -0,0 +1,39 @@
+/*
+ * Sound.h
+ *
+ *  Created on: 2013-03-06
+ *      Author: adam_000
+ */
+
+#ifndef SOUND_H_
+#define SOUND_H_
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "altera_up_sd_card_avalon_interface.h"
+#include "SoundMixer.h"
+
+#define BITS_PER_BYTE 8
+#define BITS_PER_SAMPLE_OFFSET 34
+#define DATA_LENGTH_OFFSET 40
+
+struct Sound {
+	volatile unsigned int position;
+	unsigned int length;
+	volatile int loops;
+	unsigned int *buffer;
+	volatile bool playing;
+	volatile float volume;
+};
+
+struct Sound* initSound(unsigned int);
+void updateSoundPosition(struct Sound*, int);
+struct Sound* loadWavSound(char*);
+void combineSounds(struct Sound*, struct Sound*, int, int, bool);
+void unloadSound(struct Sound*);
+void playSound(struct Sound*, float, int, int);
+void pauseSound(struct Sound*);
+void stopSound(struct Sound*);
+void clearSoundBuffer(struct Sound*);
+
+#endif /* SOUND_H_ */
