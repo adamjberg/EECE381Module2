75066abcc6fa757134766b1f3caec882f5e49826
Adam Berg <adam@xyzdigital.com>
2013-04-03 17:28:19 -0700

    Start SoundPropertyActivity and code cleanup
    When the properties button is pressed it will now open up the property
    activity.

--- Android_Workspace/ece381/src/com/example/ece381/MixerActivity.java
+++ Android_Workspace/ece381/src/com/example/ece381/MixerActivity.java
@@ -1,21 +1,14 @@
 package com.example.ece381;
 
 
-import java.util.ArrayList;
-
-import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.graphics.Canvas;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Point;
 import android.os.Bundle;
-import android.os.Debug;
 import android.util.Log;
-import android.view.DragEvent;
 import android.view.MotionEvent;
-import android.view.SurfaceView;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.View.OnTouchListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -25,15 +18,14 @@ import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
 
-public class MixerActivity extends Activity{
-	
+public class MixerActivity extends Activity {
 	public static Mix theMix;
 	private SeekBar Timeline;
+	private SeekBar globalVolumeBar;
 	public static MixerCanvas mixerCanvas;
 	private TextView textprog;
 	public static ListView songList;
-	private Point p = new Point();
-	public static ArrayList <String> songs = new ArrayList<String>();
+	public static Clip selectedClip;
 	public static int idOfSongSelected = 0;
 	public static boolean selSong = false; //true = a song is selected| false = mixelement is selected if songid =0
 	public static int indexOfSel = -1;
@@ -50,6 +42,7 @@ public class MixerActivity extends Activity{
 		refreshList();
 	}
 
+	@Override
 	public void onResume() {
 		Log.v("PlaylistAcitivty Resume", "");
 		super.onResume();
@@ -57,9 +50,22 @@ public class MixerActivity extends Activity{
 		Command.syncOpenPlaylistsPanel();
 	}
 	
+	@Override
 	public void onPause(){
 		refreshList();
 		mixerCanvas.invalidate();
+		super.onPause();
+	}
+	
+	public void createClipsFromSongs() {
+		Song[] songs = db.getSongs();
+		Song nextSong;
+		for(int i = 0; i < songs.length; i++) {
+			nextSong = songs[i];
+			if(nextSong != null) {
+				theMix.addClip(new Clip(nextSong));
+			}
+		}
 	}
 	
 	public void setUP(){
@@ -70,19 +76,19 @@ public class MixerActivity extends Activity{
 		addTimeline();
 		String temp =  "0" + " / " + Integer.toString(Timeline.getMax());
 		textprog.setText(temp);
-		listAdapter = new ArrayAdapter<String>(this, R.layout.simplerow, songs);
+		listAdapter = new ArrayAdapter<String>(this, R.layout.simplerow, db.getSongsName());
+				
+		globalVolumeBar = (SeekBar) this.findViewById(R.id.volume);
+		globalVolumeBar.setOnSeekBarChangeListener(new VolumeSeekChangeListener());
 		
 		songList.setOnItemClickListener(new OnItemClickListener(){
 
 			@Override
 			public void onItemClick(AdapterView<?> adv, View v, int index,
 					long it) {
-				// TODO Auto-generated method stub
-				selSong = true;
-				indexOfSel = index;
-				idOfSongSelected = theMix.getClipat(indexOfSel).getID();
+				selectedClip = theMix.getClipat(index);
+				idOfSongSelected = selectedClip.getID();
 				mixerCanvas.selIndex = -1;
-				System.out.println( "When Clicked" + indexOfSel + " ID:" + idOfSongSelected  );
 				mixerCanvas.invalidate();
 			}
 			
@@ -92,8 +98,7 @@ public class MixerActivity extends Activity{
 		mixerCanvas.setOnTouchListener(new OnTouchListener(){
 			@Override
 			public boolean onTouch(View v, MotionEvent mE) {
-				p.x=(int) mE.getX();
-				p.y=(int) mE.getY();
+				Point p = new Point((int) mE.getX(), (int) mE.getY());
 				p.y = decodePoint(p);
 				if(selSong = true && idOfSongSelected != 0 && indexOfSel >= 0){
 				mixerCanvas.addElement(p);
@@ -113,94 +118,60 @@ public class MixerActivity extends Activity{
 		});
 		songList.setAdapter(listAdapter); 
 		populateSongs();
-		
-	    
-		//may add more init in here
-		
+		createClipsFromSongs();
 	}
 	
 	private void addTimeline() {
 		Timeline = (SeekBar) findViewById(R.id.scroll);
 		Timeline.setMax(theMix.lengthOf() + 3000 /*30 sec*/);
 		Timeline.setProgress(0);
-		Timeline.setOnSeekBarChangeListener(new OnSeekBarChangeListener(){
-			@Override 
-			public void onProgressChanged(SeekBar Timeline, int progress,
-										  boolean fromUser) {
-				if(fromUser){
-					theMix.seekTime(progress);
-					Timeline.setMax(theMix.lengthOf() + 3000);
-					Timeline.setProgress(progress);
-					mixerCanvas.setProgress(progress);
-					String temp = Integer.toString(progress) + " / " + Integer.toString(Timeline.getMax());
-					textprog.setText(temp);
-					mixerCanvas.invalidate();
-				}
-			}
-			@Override
-			public void onStartTrackingTouch(SeekBar seekBar) {}
-			@Override
-			public void onStopTrackingTouch(SeekBar seekBar) {}
-		});
+		Timeline.setOnSeekBarChangeListener(new TimelineSeekChangeListener());
 	}
-	
-
 	
 	public int decodePoint(Point p){
 		return (p.y/100);
 	}
 	
-	public void populateSongs(){
-		songs.add("Sheep1");
-		songs.add("Sheep2");
-		songs.add("sheep3");
-		songs.add("Popsicle");
-		/*
-	    for(int i = 1; i <= db.getTotalSongs(); i++ ) {
-			   songs.add(db.getSongs()[i].getSongName());
-	    }*/
-		int temp =200;//length of song
-		
-		for(String s: songs){
-			int id = songs.indexOf(s)+1;
-			//id = getSelectedSongId(s);
-			//temp =  db.getSongs()[id].getLength();
-			theMix.addClip(new Clip(s, temp , id));
-			System.out.println( "when populating name: " + s + " ID: " + id);
+	public void populateSongs() {
+		db.addSong(new Song("Sheep1"));
+		db.addSong(new Song("Sheep2"));
+		db.addSong(new Song("Sheep3"));
+		db.addSong(new Song("Popsicle"));
+
+		Song[] songs = db.getSongs();
+		for(int i = 0; i < 4; i++) {
+			songs[i].setSize(200);
 		}
 	}
 	
-	public void onPlay(){
-		
+	public void onPlayMix(View view) {
+		theMix.play();
+	}
+
+	public void onStopMix(View view) {
+		theMix.stop();
+	}
+
+	public void onOpenProperties(View view) {
+		Intent i = new Intent(this, SongPropertyActivity.class);
+		startActivity(i);
 	}
 	
-	public void onStopPlay(){
-		
+	public void onSave(View view) {
+
 	}
-	
-	public void onSave(){
-		
-	}
-	
-	public void onDelete(){
-		if(selSong = true && idOfSongSelected != 0 ){
-			mixerCanvas.deleteAllInstanceOf(idOfSongSelected);}
-		else if(selSong = false && idOfSongSelected ==0){
+
+	public void onDelete(View view) {
+		if (selSong == true && idOfSongSelected != 0) {
+			mixerCanvas.deleteAllInstanceOf(idOfSongSelected);
+		} else if (selSong == false && idOfSongSelected == 0) {
 			mixerCanvas.deleteSelectedSeg();
 		}
-		
-		else{
-		selSong = false;
-		idOfSongSelected = 0;
+
+		else {
+			selSong = false;
+			idOfSongSelected = 0;
 		}
-	}
-	
-	public void onVolumeChange(){
-		
-	}
-	
-	public void onProperties(){
-		
 	}
 	
 	public int getSelectedSongId(String selectedValue) {
@@ -212,10 +183,57 @@ public class MixerActivity extends Activity{
 		return songid;
 	}
 	  
-	
 	public void refreshList() {
-		//listAdapter.clear();
-		//listAdapter.addAll(db.getSongsName());
-		//listAdapter.notifyDataSetChanged();
+		// listAdapter.clear();
+		// listAdapter.addAll(db.getSongsName());
+		// listAdapter.notifyDataSetChanged();
+	}
+	
+	private class VolumeSeekChangeListener implements OnSeekBarChangeListener {
+		@Override
+		public void onProgressChanged(SeekBar Timeline, int progress,
+				boolean fromUser) {
+			if (fromUser) {
+				theMix.seekTime(progress);
+				Timeline.setProgress(progress);
+				mixerCanvas.setProgress(progress);
+				String temp = Integer.toString(progress) + " / "
+						+ Integer.toString(Timeline.getMax());
+				textprog.setText(temp);
+				mixerCanvas.invalidate();
+			}
 		}
+
+		@Override
+		public void onStartTrackingTouch(SeekBar seekBar) {
+		}
+
+		@Override
+		public void onStopTrackingTouch(SeekBar seekBar) {
+		}
+	}
+	
+	private class TimelineSeekChangeListener implements OnSeekBarChangeListener {
+		@Override
+		public void onProgressChanged(SeekBar Timeline, int progress,
+				boolean fromUser) {
+			if (fromUser) {
+				theMix.seekTime(progress);
+				Timeline.setProgress(progress);
+				mixerCanvas.setProgress(progress);
+				String temp = Integer.toString(progress) + " / "
+						+ Integer.toString(Timeline.getMax());
+				textprog.setText(temp);
+				mixerCanvas.invalidate();
+			}
+		}
+
+		@Override
+		public void onStartTrackingTouch(SeekBar seekBar) {
+		}
+
+		@Override
+		public void onStopTrackingTouch(SeekBar seekBar) {
+		}
+	}
 }
